c384e8fa85e5ca6f4d23920f46005a1eb9172d12
Dmitrii Pichulin <deem@deem.ru>
2026-03-18 23:25:16 +0300

    fix path

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once 'config.php';
 $z = (int)( $_COOKIE['z'] ?? 180 ); // TIMEZONE
 
 if( isset( $_SERVER['REQUEST_URI'] ) )
-    $urio = substr( $_SERVER['REQUEST_URI'], strlen( W8IO_ROOT ) );
+    $urio = substr( strtok( $_SERVER['REQUEST_URI'], '?' ), strlen( W8IO_ROOT ) );
 else
     $urio = '';
 
